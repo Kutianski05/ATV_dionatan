e00a2838dddd915a9e26841f4d20ad395eeed5ec
João Vitor <engs-joaokutianski@camporeal.edu.br>
2026-04-27 19:30:29 -0300

    .

--- js/atividade1.js
+++ js/atividade1.js
@@ -3,4 +3,10 @@ const botao = doucument.getElementById('botao')
 
 botao.addEventListener("click", function(){
     titulo.innerText = "Outro texto"
-});
+
+    if(titulo.innerText == "Outro texto"){
+        titulo.innerText = "45"
+    } else {
+        titulo.innerText = "Outro texto"
+    }
+})
