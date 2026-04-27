1fbb138dcde8aafdf76781f2ce45156378832c94
João Vitor <engs-joaokutianski@camporeal.edu.br>
2026-04-27 19:33:20 -0300

    .

--- js/atividade1.js
+++ js/atividade1.js
@@ -1,5 +1,5 @@
-const titulo = document.getElementById('titulo')
-const botao = doucument.getElementById("botao")
+const titulo = document.getElementById('titulo');
+const botao = doucument.getElementById("botao");
 
 botao.addEventListener("click", function(){
     titulo.innerText = "Outro texto"
