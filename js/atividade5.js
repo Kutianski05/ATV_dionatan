49099bf514205ff07b482f7748abc8d3391ba683
João Vitor <engs-joaokutianski@camporeal.edu.br>
2026-04-28 19:06:28 -0300

    .

--- js/atividade5.js
+++ js/atividade5.js
@@ -29,3 +29,27 @@
 // → chamar salvar() ao:
 //    - adicionar item
 //    - Carregar a página
+
+const botao = document.getElementById("botao");
+const input = document.getElementById("texto");
+const lista = document.getElementById("lista");
+
+
+botao.addEventListener('click', function(){
+    
+    const texto = input.value;
+    
+    const item = document.createElement("li");
+    item.textContent = texto;
+    lista.appendChild(item);
+
+    const btnRemover = document.createElement("button");
+    btnRemover.textContent = "Remover";
+
+    btnRemover.addEventListener("click" , function(){
+        lista.removeChild(item);
+    });
+
+    item.appendChild(btnRemover);
+
+});
